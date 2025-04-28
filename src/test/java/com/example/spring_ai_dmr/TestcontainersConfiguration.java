ac425f96b68257b3570ade96ea913120ea877abd
Eddú Meléndez <eddu.melendez@gmail.com>
2025-04-28 13:50:09 -0600

    Use DockerModelRunnerContainer

--- src/test/java/com/example/spring_ai_dmr/TestcontainersConfiguration.java
+++ src/test/java/com/example/spring_ai_dmr/TestcontainersConfiguration.java
@@ -3,22 +3,20 @@ package com.example.spring_ai_dmr;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistrar;
-import org.testcontainers.containers.SocatContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.DockerModelRunnerContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
     @Bean
-    SocatContainer socat() {
-        return new SocatContainer(DockerImageName.parse("alpine/socat:1.8.0.1"))
-                .withTarget(80, "model-runner.docker.internal");
+    DockerModelRunnerContainer socat() {
+        return new DockerModelRunnerContainer("alpine/socat:1.8.0.1");
     }
     
     @Bean
-    DynamicPropertyRegistrar properties(SocatContainer socat) {
+    DynamicPropertyRegistrar properties(DockerModelRunnerContainer dmr) {
         return (registrar) -> {
-            registrar.add("spring.ai.openai.base-url", () -> "http://%s:%d/engines".formatted(socat.getHost(), socat.getMappedPort(80)));
+            registrar.add("spring.ai.openai.base-url", dmr::getOpenAIEndpoint);
             registrar.add("spring.ai.openai.api-key", () -> "test-api-key");
             registrar.add("spring.ai.openai.chat.options.model", () -> "ai/gemma3");
         };
